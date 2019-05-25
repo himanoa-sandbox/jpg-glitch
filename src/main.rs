cfbdf6ff0c62c6e725df0c3e6efd20086d6bd35d
himanoa <matsunoappy@gmail.com>
2019-05-26 02:59:38 +0900

    Implement load and parse jpg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,177 @@
-fn main() {
-    println!("Hello, world!");
+extern crate clap;
+#[macro_use] extern crate failure;
+
+use std::io::prelude::*;
+use std::io::{BufReader, Cursor};
+use std::fs::File;
+use std::cell::RefCell;
+use std::fmt;
+
+#[derive(Debug, Fail)]
+enum JPGError {
+    #[fail(display = "SOI is not found error")]
+    SOINotFoundError,
+    #[fail(display = "APP0 is not found error")]
+    APP0NotFoundError,
+    #[fail(display = "DQT is not found error")]
+    DQTNotFoundError,
+    #[fail(display = "SOF0 is not found error")]
+    SOF0NotFoundError,
+    #[fail(display = "DHT is not found error")]
+    DHTNotFoundError,
+    #[fail(display = "SOS is not found error")]
+    SOSNotFoundError,
+    #[fail(display = "ImageBody is not found error")]
+    ImageBodyNotFound,
+    #[fail(display = "Broken Jpeg file")]
+    BrokenJpgFileError
+}
+
+use clap::{App, Arg};
+
+struct JPG {
+    pub soi: RefCell<[u8;2]>,
+    pub app0: RefCell<[u8;18]>,
+    pub dqt: RefCell<[u8;69 * 2]>,
+    pub sof0: RefCell<[u8;19]>,
+    pub dht: RefCell<[u8;33 + 183 + 33 + 183]>,
+    pub sos: RefCell<[u8;14]>,
+    pub image: RefCell<std::vec::Vec<u8>>,
+}
+
+impl fmt::Display for JPG {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        fn formatter(b: &u8) -> String {
+            format!("{:x} ", b)
+        }
+        let soi = &self.soi.borrow().iter().map(formatter).collect::<String>();
+        let app0 = &self.app0.borrow().iter().map(formatter).collect::<String>();
+        let dqt = &self.dqt.borrow().iter().map(formatter).collect::<String>();
+        let sof0 = &self.sof0.borrow().iter().map(formatter).collect::<String>();
+        let dht = &self.dht.borrow().iter().map(formatter).collect::<String>();
+        let sos = &self.sos.borrow().iter().map(formatter).collect::<String>();
+        let image = &self.image.borrow().iter().map(formatter).collect::<String>();
+        write!(
+            f,
+            "
+soi: {}
+app0: {}
+dqt: {}
+sof0: {}
+dht: {}
+sos: {}
+image: {}
+            ",
+            soi,
+            app0,
+            dqt,
+            sof0,
+            dht,
+            sos,
+            image
+        )
+    }
+}
+
+
+fn parse(binary: &std::vec::Vec<u8>) -> Result<JPG, JPGError> {
+    let mut cur = Cursor::new(binary);
+
+    let mut soi_buffer = [false as u8;2];
+    let mut app0_buffer = [false as u8;18];
+    let mut dqt_buffer = [false as u8 ;69 * 2];
+    let mut sof0_buffer = [false as u8; 19];
+    let mut dht_buffer = [false as u8; 33 + 183 + 33 + 183];
+    let mut sos_buffer = [false as u8;14];
+    let mut other_buffer = Vec::new();
+
+    match cur.read_exact(&mut soi_buffer) {
+        Ok(_) => {}
+        Err(_) => return Err(JPGError::SOINotFoundError)
+    };
+    match cur.read_exact(&mut app0_buffer) {
+        Ok(_) => {}
+        Err(_) => return Err(JPGError::APP0NotFoundError)
+    };
+    match cur.read_exact(&mut dqt_buffer) {
+        Ok(_) => {}
+        Err(_) => return Err(JPGError::DQTNotFoundError)
+    }
+    match cur.read_exact(&mut sof0_buffer) {
+        Ok(_) => {}
+        Err(_) => return Err(JPGError::SOF0NotFoundError)
+    };
+    match cur.read_exact(&mut dht_buffer) {
+        Ok(_) => {}
+        Err(_) => return Err(JPGError::DHTNotFoundError)
+    };
+    match cur.read_exact(&mut sos_buffer) {
+        Ok(_) => {}
+        Err(_) => return Err(JPGError::SOSNotFoundError)
+    };
+    match cur.read_to_end(&mut other_buffer) {
+        Ok(_) => {}
+        Err(_) => return Err(JPGError::ImageBodyNotFound)
+    };
+    let buffer_iter = &mut other_buffer.into_iter();
+    let mut image_body: Vec<u8> = Vec::new();
+
+    while let (Some(byte), next_byte) = (buffer_iter.next(), buffer_iter.next()) {
+        match (byte, next_byte) {
+            (b, Some(nb)) => {
+                if b == 255 as u8 && nb == 217 as u8 {
+                    break;
+                } else {
+                    image_body.push(b);
+                    image_body.push(nb);
+                }
+            }
+            (_, None) => {
+                return Err(JPGError::BrokenJpgFileError)
+            }
+        }
+    }
+
+    return Ok(JPG {
+        soi: RefCell::new(soi_buffer),
+        app0: RefCell::new(app0_buffer),
+        dqt: RefCell::new(dqt_buffer),
+        sof0: RefCell::new(sof0_buffer),
+        dht: RefCell::new(dht_buffer),
+        sos: RefCell::new(sos_buffer),
+        image: RefCell::new(image_body)
+    });
+}
+
+fn brakeJpg(jpg: JPG) -> i64 {
+    12
+}
+
+fn main() -> std::io::Result<()> {
+    let matches = App::new("jpg-glitch")
+        .version("1.0")
+        .author("himanoa <matsunoappy@gmail.com>")
+        .arg(
+            Arg::with_name("INPUT")
+            .help("Sets the input file to use")
+            .required(true)
+            .index(1),
+            )
+        .get_matches();
+    match matches.occurrences_of("v") {
+        0 => println!("No verbose info"),
+        1 => println!("Some verbose info"),
+        2 => println!("Tons of verbose info"),
+        3 | _ => println!("Don't be crazy"),
+    }
+    let f = File::open(matches.value_of("INPUT").unwrap())?;
+    let mut buffer = vec![];
+    let mut reader = BufReader::new(f);
+    reader.read_to_end(&mut buffer)?;
+    let a = parse(&buffer).unwrap();
+    println!("{}", a);
+    brake();
+
+    Ok(())
+
 }
